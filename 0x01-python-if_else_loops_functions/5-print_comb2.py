89d473c4ed18a5584501b66fdbca88dfe08f5449
Birhanus <birhanushimles716@gmail.com>
2021-12-17 07:00:05 -0800

    looos and functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{0:02d}".format(i), end=", ")
-print("{:d}".format(99))
+charend = ", "
+for i in range(0, 100):
+    if i < 10:
+        print("{}{}".format(0, i), end=charend)
+    else:
+        if i == 99:
+            charend = "\n"
+        print(i, end=charend)
