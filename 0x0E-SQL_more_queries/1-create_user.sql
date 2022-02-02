bff81c6ff401d5e0b52421b93db3a209720ace6a
birhanus <birhanushimles716@gmail.com>
2022-02-02 17:44:14 +0000

    create root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- sql script that creates the MYSQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
+-- the user_0d_1 password should be set to user_0d_pwd
+-- if the user user_0d_1 already exists, your script should not fail
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES  ON * . * TO 'user_0d_1'@'localhost';
