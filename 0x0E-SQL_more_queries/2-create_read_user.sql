245b72270de79ef30c115797e3681910e6db3bed
birhanus <birhanushimles716@gmail.com>
2022-02-02 17:56:31 +0000

    create databse

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- sql script that crates the database hbtn_0d_2 and the user_0d_2
+-- user_0d_2 should have only SELECT pribilege int the database hbtn_0d_2
+-- if the database hbtn_0d_2 already exists, your script should not fail
+-- if the user  user_0d_2 already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost'
