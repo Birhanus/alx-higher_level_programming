ea6d1ae3351d14aee2d6511eab43e214be3ae27b
birhanus <birhanushimles716@gmail.com>
2022-02-02 19:27:49 +0000

    databse

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,8 +4,9 @@
 -- If the database hbtn_0d_use already exists, your script should not fail.
 -- If the table states already exists, your script should not fail.
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `states`(
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL
 );
