8c0a9cca96cb7806d7714010f466f85e90584648
birhanus <birhanushimles716@gmail.com>
2022-02-02 18:06:06 +0000

    flush privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,9 @@
 -- sql script that crates the database hbtn_0d_2 and the user_0d_2
--- user_0d_2 should have only SELECT pribilege int the database hbtn_0d_2
+-- user_0d_2 should have only SELECT priVilege int the database hbtn_0d_2
 -- if the database hbtn_0d_2 already exists, your script should not fail
 -- if the user  user_0d_2 already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost'
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
