ba4693bf7bcade88fbd635f4395983fda26b5965
birhanus <birhanushimles716@gmail.com>
2022-02-02 17:35:09 +0000

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- sql script that creates the MYSQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
-GRANT ALL PRIVILEGES  ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES  ON * . * TO 'user_0d_1'@'localhost';
