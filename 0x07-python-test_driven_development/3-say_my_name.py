f4df9dda6b89c60996ca7358a325b955aa419724
birhanus <birhanushimles716@gmail.com>
2022-02-10 23:11:25 +0000

    say may name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-'''
-module: say_my_name
-'''
+"""
+This is the "3-say_my-name" module.
+The 3-say_my_name  module supplies one function, say_my_name.
+"""
 
 
-def say_my_name(first_name, last_name=''):
-    ''' print first and last name, make the big bucks
-    Keyword arguments:
-    first_name -- string
-    last_name -- string
-    '''
-
-    #  ERROR MESSAGE DICT  #
-    err_msg = {}
-    err_msg["FirstNotStr"] = "first_name must be a string"
-    err_msg["LastNotStr"] = "last_name must be a string"
-
-    #  TESTS  #
-    if type(first_name) != str:
-        raise TypeError(err_msg["FirstNotStr"])
-    if type(last_name) != str:
-        raise TypeError(err_msg["LastNotStr"])
-
-    #  OUTPUT  #
-    print("My name is {}".format(first_name), end='')
-    if len(last_name) == 0:
-        print()
-    else:
-        print(" {}".format(last_name))
+def say_my_name(first_name, last_name=""):
+    """Prints "My name is" followed by the first name and optional last name"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
