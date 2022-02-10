877f82f2814154100cce701ebdf6fe5746c754cf
birhanus <birhanushimles716@gmail.com>
2022-02-10 23:03:59 +0000

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # 2-matrix_divided.py
-"""Defines a matrix division function."""
+"""
+Defines a matrix division function.
+
+"""
 
 
 def matrix_divided(matrix, div):
@@ -33,4 +36,3 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
-
