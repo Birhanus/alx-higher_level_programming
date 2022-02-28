a396142e16b0b24a5957c31526c21399cd6ad26d
birhanus <birhanushimles716@gmail.com>
2022-02-28 19:20:08 +0000

    An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,6 +2,8 @@
 
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
+  process.exit(1);
 } else {
-  console.log('My number:' + process.argv[2]);
+  console.log('My number: ' + process.argv[2]);
+  process.exit(1);
 }
