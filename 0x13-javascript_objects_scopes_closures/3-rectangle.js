0dc5f1c9860d8fbacdbc09010a328b0ea4b780bd
birhanus <birhanushimles716@gmail.com>
2022-03-01 13:29:55 +0000

    Rectangle#3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        process.stdout.write('x');
+      }
+      console.log();
+    }
+  }
+};
