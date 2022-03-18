ffce45cbcffbb579b02a46f15cf031f03dc7c4b4
birhanus <birhanushimles716@gmail.com>
2022-03-18 15:07:26 +0000

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@
 if __name__ == "__main__":
     from urllib import parse, request
     from sys import argv
-    d = {'email': argv[2]}
+    values = {'email': argv[2]}
     data = parse.urlencode(values).encode('utf-8')
     r = request. Request(argv[1], data)
     with request.urlopen(r) as a:
